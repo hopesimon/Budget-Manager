c05a88b7ce48db3ebb75577751f5aa9dd9ea321f
Hope Simon <hopetsimon@gmail.com>
2018-09-25 14:57:50 -0400

    Repair broken links

--- admin/admin.php
+++ admin/admin.php
@@ -133,7 +133,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">ASAB Admin</a>
+                <a class="navbar-brand" href="/">ASAB Admin</a>
             </div>
             <!-- /.navbar-header -->
 
--- admin/forms.php
+++ admin/forms.php
@@ -196,7 +196,7 @@ elseif(isset($_POST['deny'])){
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">ASAB Admin</a>
+                <a class="navbar-brand" href="/">ASAB Admin</a>
             </div>
             <!-- /.navbar-header -->
 
