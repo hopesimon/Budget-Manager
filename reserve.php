563c677678d337790c097f6557a7c70d162164ca
Hope Simon <hopetsimon@gmail.com>
2018-09-24 20:37:38 -0400

    Remove extensions

--- reserve.php
+++ reserve.php
@@ -155,7 +155,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-fau fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="reserve.php">Budget Request Form</a>
+            <a class="navbar-brand" href="reserve">Budget Request Form</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
